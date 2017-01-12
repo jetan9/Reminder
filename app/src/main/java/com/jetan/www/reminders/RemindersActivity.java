7880be56d35f4b5153843e5e9dc0566bdf71b194
An Ningyu <anningyu@we.com>
2017-01-12 15:03:23 +0800

    Adds new reminder and exit menu options.

--- app/src/main/java/com/jetan/www/reminders/RemindersActivity.java
+++ app/src/main/java/com/jetan/www/reminders/RemindersActivity.java
@@ -2,6 +2,10 @@ package com.jetan.www.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,5 +28,26 @@ public class RemindersActivity extends AppCompatActivity {
                 }
         );
         mListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_reminders, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                Log.d(getLocalClassName(), "create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
