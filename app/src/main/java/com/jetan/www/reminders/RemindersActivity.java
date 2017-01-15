b907a68abcbd067f4e9053908ffaf6a89e6664c6
An Ningyu <anningyu@we.com>
2017-01-15 20:25:01 +0800

    Adds database Create, Read, Update and Delete support with custom dialogs.

--- app/src/main/java/com/jetan/www/reminders/RemindersActivity.java
+++ app/src/main/java/com/jetan/www/reminders/RemindersActivity.java
@@ -13,10 +13,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RemindersActivity extends AppCompatActivity {
@@ -60,10 +67,13 @@ public class RemindersActivity extends AppCompatActivity {
                 modelListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        int nId = getIdFromPosition(masterListPosition);
                         if (position == 0) {
-                            Toast.makeText(RemindersActivity.this, "edit " + masterListPosition, Toast.LENGTH_SHORT).show();
+                            Reminder reminder = dbAdapter.fetchReminderById(nId);
+                            fireCustomDialog(reminder);
                         } else {
-                            Toast.makeText(RemindersActivity.this, "delete " + masterListPosition, Toast.LENGTH_SHORT).show();
+                            dbAdapter.deleteReminderById(nId);
+                            cursorAdapter.changeCursor(dbAdapter.fetchAllReminders());
                         }
                         dialog.dismiss();
                     }
@@ -123,6 +133,50 @@ public class RemindersActivity extends AppCompatActivity {
         }
     }
 
+    private void fireCustomDialog(final Reminder reminder) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_custom);
+
+        TextView titleView = (TextView)dialog.findViewById(R.id.custom_title);
+        final EditText editCustom = (EditText)dialog.findViewById(R.id.custom_edit_reminder);
+        Button commitButton = (Button)dialog.findViewById(R.id.custom_button_commit);
+        final CheckBox checkBox = (CheckBox)dialog.findViewById(R.id.custom_check_box);
+        LinearLayout rootLayout = (LinearLayout)dialog.findViewById(R.id.custom_root_layout);
+        final boolean isEditOperation = (reminder != null);
+
+        if (isEditOperation) {
+            titleView.setText("Edit Reminder");
+            checkBox.setChecked(reminder.getImportant() == 1);
+            editCustom.setText(reminder.getContent());
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.blue));
+        }
+
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String reminderText = editCustom.getText().toString();
+                if (isEditOperation) {
+                    Reminder reminderEdited = new Reminder(reminder.getId(),
+                            reminderText, checkBox.isChecked() ? 1: 0);
+                    dbAdapter.updateReminder(reminderEdited);
+                } else {
+                    dbAdapter.createReminder(reminderText, checkBox.isChecked());
+                }
+                cursorAdapter.changeCursor(dbAdapter.fetchAllReminders());
+                dialog.dismiss();
+            }
+        });
+
+        Button buttonCancel = (Button)dialog.findViewById(R.id.custom_button_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -134,7 +188,7 @@ public class RemindersActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_new:
-                Log.d(getLocalClassName(), "create new Reminder");
+                fireCustomDialog(null);
                 return true;
             case R.id.action_exit:
                 finish();
