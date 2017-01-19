fdb7ccd1e7d47320e8d5821211e3bf51122a7a2a
An Ningyu <anningyu@we.com>
2017-01-19 11:33:44 +0800

    Adds BroadcastReceiver for alarms.

--- app/src/main/java/com/jetan/www/reminders/ReminderAlarmReceiver.java
+++ app/src/main/java/com/jetan/www/reminders/ReminderAlarmReceiver.java
@@ -1,8 +1,15 @@
 package com.jetan.www.reminders;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import java.util.Date;
 
 /**
  * Created by anningyu on 2017-01-17.
@@ -10,8 +17,21 @@ import android.content.Intent;
 public class ReminderAlarmReceiver extends BroadcastReceiver {
     public static final String REMINDER_TEXT = "REMINDER_TEXT";
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        String reminderText = intent.getStringExtra(REMINDER_TEXT);
+        Intent intentAction = new Intent(context, RemindersActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(context, 0, intentAction, 0);
+        Notification notification = new Notification.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("Reminder!")
+                .setWhen(new Date().getTime())
+                .setContentText(reminderText)
+                .setContentIntent(pi)
+                .build();
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
